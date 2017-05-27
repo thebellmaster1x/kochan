6b3167b1ee1818778ffb405be6c59212e73a2a58
thebellmaster1x <mattcccp@gmail.com>
2017-05-27 18:57:42 -0500

    part of chapter 11

---
+++ ch11/11.2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+struct entry {
+    int             value;
+    struct entry    *next;
+};
+
+void insertEntry( struct entry *oldEntry, struct entry *newEntry );
+
+int main (void) {
+
+    struct entry p1, p2, p3;
+    struct entry *listPointer = &p1;
+
+    p1.value = 1;
+    p1.next = &p2;
+
+    p2.value = 2;
+    p2.next = NULL;
+
+    p3.value = 3;
+    p3.next = NULL;
+
+    insertEntry( &p1, &p3 );
+
+    while( listPointer ) {
+        printf("%d\n", listPointer->value);
+        listPointer = listPointer->next;
+    }
+
+    return 0;
+
+}
+
+void insertEntry( struct entry *oldEntry, struct entry *newEntry ) {
+
+    newEntry->next = oldEntry->next;
+    oldEntry->next = newEntry;
+
+}
